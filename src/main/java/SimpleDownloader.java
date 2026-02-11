c056e49064ff2b7b982b4602eab8132da1f9c2b9
Masood2000 <masudurrehman2000@gmail.com>
2026-02-11 01:49:34 +0100

    simple downloader

---
+++ src/main/java/SimpleDownloader.java
@@ -0,0 +1,36 @@
+/***
+ * @Author: Masood
+ * @Date: 2026-02-11
+ * @Description: Simple Downloader class in java
+ */
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SimpleDownloader {
+
+    public void downloadFile(String fileURL, String outputPath) throws Exception {
+
+        URL url = new URL(fileURL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        try (InputStream in = connection.getInputStream();
+             FileOutputStream out = new FileOutputStream(outputPath)) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        }
+
+        System.out.println("Download complete: " + outputPath);
+    }
+
+
+}
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,30 @@
 package com.masood
 
 import FileMetadata
+import SimpleDownloader
 import java.io.File
+import java.util.Date
 
 fun main() {
 
 
-    var url = "http://localhost:8080/abc.txt"
-    val fileMetadata = FileMetadata();
+    val url = "http://localhost:8080/abc.txt"
 
+    val date = Date()
+    val time = date.toLocaleString().replace(" ","-").replace(",","").replace(":","-")
+
+    val outputFile = File("D:/Learning/jetbrains/Integrating reporting SDK into web frontends/server/downloads/")
+    val outputFilePath = outputFile.absolutePath  +"/downloaded-file-" + time +".txt";
+
+
+    val fileMetadata = FileMetadata();
+    val simpleDownloader = SimpleDownloader();
 
     fileMetadata.getMetaData(url);
+
+
+    simpleDownloader.downloadFile(url, outputFilePath)
+
 
 
 }
